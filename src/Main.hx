bb5596a77b8964ed4583cf57dad9793f425d2e0f
Dolf <eric.jonsson.hederstedt@gmail.com>
2025-08-09 15:13:43 +0200

    format and launc

--- src/Main.hx
+++ src/Main.hx
@@ -3,27 +3,26 @@ package;
 import engine.*;
 
 class Main {
-    public static function main() {
-        Engine.start();
+	public static function main() {
+		Engine.start();
 
-        #if js
-        var loop = null;
-        loop = (_) -> {
-            mainLoop();
-            if (!Engine.should_stop)
-                js.Browser.window.requestAnimationFrame(loop);
-        };
-        js.Browser.window.requestAnimationFrame(loop);
-        #else
-        while (!Engine.should_stop)
-        {
-            mainLoop();
-            Sys.sleep(1 / 60);
-        }
-        #end
-    }
+		#if js
+		var loop = null;
+		loop = (_) -> {
+			mainLoop();
+			if (!Engine.should_stop)
+				js.Browser.window.requestAnimationFrame(loop);
+		};
+		js.Browser.window.requestAnimationFrame(loop);
+		#else
+		while (!Engine.should_stop) {
+			mainLoop();
+			Sys.sleep(1 / 60);
+		}
+		#end
+	}
 
-    public static function mainLoop() {
-        trace("fuck");
-    }
+	public static function mainLoop() {
+		trace("fuck");
+	}
 }
