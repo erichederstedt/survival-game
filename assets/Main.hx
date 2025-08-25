155f91420527f705ff26bdd921085e02df5ac046
eric <eric.jonsson-hederstedt@hacksawstudios.com>
2025-08-25 13:43:34 +0200

    Fixed up texture pre-processor

--- assets/Main.hx
+++ assets/Main.hx
@@ -7,25 +7,17 @@ import sys.io.Process;
 class Main {
 	public static function main() {
 		var assetsrc = "./assetsrc";
-		final imageFormats = [".png"];
+		final imageFormats = ["png"];
 		final imagesToEncode:Array<String> = [];
-		if (FileSystem.exists(assetsrc) && FileSystem.isDirectory(assetsrc)) {
-			var files = FileSystem.readDirectory(assetsrc).filter((fileName:String) -> {
-				return fileName.length > 4 && imageFormats.contains(fileName.substr(fileName.length - 4, 4));
-			});
-
-			for (fileName in files) {
-				var fullPath = Path.join([assetsrc, fileName]);
-				trace('Found: $fullPath');
-				imagesToEncode.push(fullPath);
-			}
-		} else {
-			trace('Directory does not exist: $assetsrc');
-		}
+		findFiles(assetsrc, imagesToEncode, (file:Path) -> imageFormats.contains(file.ext));
 
 		for (image in imagesToEncode) {
+			final imagePath = new Path(image);
 			try {
-				final process = new Process('npx gputexenc -i ${image} -t "BC1" --basis.ktx2 true');
+				final directory = 'bin/${imagePath.dir}';
+				createDir(directory);
+
+				final process = new Process('npx texture-compressor -i ${image} -t s3tc -c DXT1 -q normal -o ${directory}/${imagePath.file}.dxt1.ktx -m -vb');
 
 				final output = process.stdout.readAll().toString();
 				Sys.print(output);
@@ -38,6 +30,32 @@ class Main {
 			} catch (e:Dynamic) {
 				trace('Error executing command: $e');
 			}
+		}
+	}
+
+	public static function createDir(dir:String) {
+		if (FileSystem.exists(dir) && FileSystem.isDirectory(dir))
+			return;
+
+		FileSystem.createDirectory(dir);
+	}
+
+	public static function findFiles(path:String, outFiles:Array<String>, ?filter:(file:Path) -> Bool) {
+		if (!FileSystem.exists(path)) {
+			return;
+		}
+
+		if (FileSystem.isDirectory(path)) {
+			for (item in FileSystem.readDirectory(path)) {
+				var fullPath = Path.join([path, item]);
+				findFiles(fullPath, outFiles, filter);
+			}
+		} else {
+			trace("Found: " + path);
+			if (filter != null && filter(new Path(path)))
+				outFiles.push(path);
+			else if (filter == null)
+				outFiles.push(path);
 		}
 	}
 }
